dd8a27a048df3f445d8fbd7af9e1f1786dc1bfd5
Kelly Do <kellydo8467@gmail.com>
2024-05-07 16:12:55 -0400

    made the navbar exclusive to home and about only

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,14 @@
+import Navbar from "../Navbar"
+import "../styles/Navbar.css"
+
 const About = () => {
     return (
+    <body>
+    <Navbar />
     <div className = "home-container">
         <p> hi wassp</p>
     </div>
+    </body>
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, NavLink, Link } from 'react-router-dom';
 import "../styles/Home.css"
+import Navbar from "../Navbar"
+import "../styles/Navbar.css"
 
 const Home = () => {
     return(
-    <div className = "home-container">
-        <div className = "slogan">
-            <p>Track your spending, master your finances</p>
-            <div className = "start">
-                <Link to = "/"> Get Started</Link>
+        <body>
+        <Navbar />
+        <div className = "home-container">
+            <div className = "slogan">
+                <p>Track your spending, master your finances</p>
+                <div className = "start">
+                    <Link to = "/"> Get Started</Link>
+                </div>
+            </div>
+
+            <div className = "dashboard">
+                <img src="https://i.ibb.co/sJmFfMK/image.png" alt = "dashboard"></img>
             </div>
         </div>
-
-        <div className = "dashboard">
-            <img src="https://i.ibb.co/sJmFfMK/image.png" alt = "dashboard"></img>
-        </div>
-    </div>
+        </body>
     );
 }
  
