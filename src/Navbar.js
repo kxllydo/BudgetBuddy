70f55cc0f45d433ac0e9e7c3c255a806b3837813
Kelly Do <kellydo8467@gmail.com>
2024-05-07 11:02:56 -0400

    basic nav bar

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <h1>Expenses</h1>
+        <div className="links">
+            <a href="/">About</a>
+            <a href="/create">Sign Up</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
