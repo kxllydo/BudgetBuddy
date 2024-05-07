e92bb57864eedf1054ec3aa3dca379c1958623d5
Kelly Do <kellydo8467@gmail.com>
2024-05-07 15:23:43 -0400

    home page done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './styles/App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
 import Navbar from './Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
-      <div className="content">
+       <Home></Home>
+      {/* <div className="content">
         <h1>App Component</h1>
-      </div>
+      </div> */}
     </div>
 
   );
