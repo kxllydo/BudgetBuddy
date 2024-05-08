b905904c0eb1b73504c2f8d0ac6ebee94b0664ce
Kelly Do <kellydo8467@gmail.com>
2024-05-08 15:30:52 -0400

    spent category divs

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,10 +1,41 @@
 import Sidebar from "./Sidebar";
+import "../styles/Dashboard.css";
 const Dashboard = () => {
     return(
-        <body>
+        <div className = "dash-container">
             <Sidebar />
-            <p>hi</p>
-        </body>
+            <div className = 'dashboard-body'>
+                <div className = "graph">
+                    <h1>hi</h1>
+                    <h2>h iihir</h2>
+                    <h2>h iihir</h2>
+                    <h2>h iihir</h2>
+                    <h2>h iihir</h2>
+
+                </div>
+                <div className = "pie-chart">
+
+                </div>
+
+                <div className = "spent">
+                    <div className = "category1">
+                        <p className = "category">Spent the most in food this month</p>
+                        <h1 className = "cost">
+                            $15.11
+                        </h1>
+                    </div>
+                    <div className = "category2">
+                        <p className = "category">Spent the most in food this month</p>
+                        <h1 className = "cost">
+                            $705.11
+                        </h1>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
     );
 }
  
--- frontend/src/pages/Sidebar.js
+++ frontend/src/pages/Sidebar.js
@@ -1,10 +1,31 @@
+import "../styles/Sidebar.css";
+import { Link } from 'react-router-dom';
+
+
 const Sidebar = () => {
     return(
-        <div className="container">
-          <nav>
-            <p> wassup</p>
+        <nav className="sidebar">
+            <div className = "top-options">
+                <div className="option">
+                <Link to ="/dashboard">Dashboard</Link>
+                </div>
+                <div className="option">
+                <Link to ="/activity">Activity</Link>
+                </div>
+                <div className="option">
+                <Link to ="/report">Reports</Link>
+                </div>
+            </div>
+
+            <div className = "bottom-options">
+                <div className="option">
+                    <Link to ="/settings">Settings</Link>
+                </div>
+                <div className="option">
+                    <Link to ="/">Log Out</Link>
+                </div>
+            </div>
         </nav>
-        </div>
     );
 }
  
