f01901841ce05ee09738c02853a7a8554b967744
Kelly Do <kellydo8467@gmail.com>
2024-05-10 14:51:49 -0400

    icons

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,33 @@
 import Sidebar from "./Sidebar";
 import "../styles/Dashboard.css";
 import PieChartComponent from "./Piechart";
-import SimpleBarChart from "./Barchart";
-import SimpleLineChart from "./Linechart";
+// import SimpleBarChart from "./Barchart";
+import SimpleLineChart from "./Linechart"
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+  { name: 'Jan', uv: 400 },
+  { name: 'Feb', uv: 300 },
+  { name: 'Mar', uv: 200 },
+  { name: 'Apr', uv: 278 },
+  { name: 'May', uv: 189 },
+  { name: 'Jun', uv: 239 },
+];
+
+const SimpleBarChart = () => {
+  return (
+    <BarChart width={600} height={240} data={data} margin={{ top: 45, right: 30, left: 20, bottom: 5 }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="uv" fill="#71b571" />
+    </BarChart>
+  );
+};
+
 
 const Dashboard = () => {
     return(
--- frontend/src/pages/Sidebar.js
+++ frontend/src/pages/Sidebar.js
@@ -1,24 +1,35 @@
 import "../styles/Sidebar.css";
 import { Link } from 'react-router-dom';
 
-
 const Sidebar = () => {
     return(
         <nav className="sidebar">
             <div className = "top-options">
                 <div className="option">
-                <Link to ="/dashboard">Dashboard</Link>
+                    <div className="sidebar-icons">
+                        <img src="https://i.ibb.co/wwXCKnB/image-removebg-preview.png" alt="dashboard" />
+                    </div>
+                    <Link to ="/dashboard">Dashboard</Link>
                 </div>
                 <div className="option">
-                <Link to ="/activity">Activity</Link>
+                    <div className="sidebar-icons">
+                            <img src="https://i.ibb.co/MfzpqJZ/image-removebg-preview-1.png" alt="activity" />
+                        </div>
+                    <Link to ="/activity">Activity</Link>
                 </div>
                 <div className="option">
-                <Link to ="/reports">Reports</Link>
+                    <div className="sidebar-icons">
+                        <img src="https://i.ibb.co/QX8T7qn/image-removebg-preview-2.png" alt="report" />
+                    </div>
+                    <Link to ="/reports">Reports</Link>
                 </div>
             </div>
 
             <div className = "bottom-options">
                 <div className="option">
+                    <div className="sidebar-icons">
+                        <img src="https://i.ibb.co/1mywcVC/image-removebg-preview-3.png" alt="settings" />
+                    </div>
                     <Link to ="/settings">Settings</Link>
                 </div>
                 <div className="option">
