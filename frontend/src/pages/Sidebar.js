be58fce9bc09a330bd9bfb9b87dfa2f491127fd4
steviels <118238179+steviels@users.noreply.github.com>
2024-05-15 10:25:12 -0400

    sidebar update

--- frontend/src/pages/Sidebar.js
+++ frontend/src/pages/Sidebar.js
@@ -1,43 +1,28 @@
 import "../styles/Sidebar.css";
 import { Link } from 'react-router-dom';
 
+const Option = ({title, link, image}) => {
+    return (
+        <div className = "option">
+            <div className = "sidebar-icons"><img src = {image} /></div>
+            <Link to = {link}>{title}</Link>
+        </div>
+    )
+}
+
 const Sidebar = () => {
     return(
         <nav className="sidebar">
             <div className = "top-options">
-                <div className="option">
-                    <div className="sidebar-icons">
-                        <img src="https://i.ibb.co/wwXCKnB/image-removebg-preview.png" alt="dashboard" />
-                    </div>
-                    <Link to ="/dashboard">Dashboard</Link>
-                </div>
-                <div className="option">
-                    <div className="sidebar-icons">
-                            <img src="https://i.ibb.co/MfzpqJZ/image-removebg-preview-1.png" alt="activity" />
-                        </div>
-                    <Link to ="/activity">Activity</Link>
-                </div>
-                <div className="option">
-                    <div className="sidebar-icons">
-                        <img src="https://i.ibb.co/QX8T7qn/image-removebg-preview-2.png" alt="report" />
-                    </div>
-                    <Link to ="/reports">Reports</Link>
-                </div>
-                <div className="option">
-                    <Link to = "/budget">Budget</Link>
-                </div>
+                <Option title = "Dashboard" link = "/dashboard" image = {require("../images/sidebar/dashboard.png")} />
+                <Option title = "Activity" link = "/activity" image = {require("../images/sidebar/activity.png")} />
+                <Option title = "Reports" link = "/reports" image = {require("../images/sidebar/reports.png")} />
+                <Option title = "Budget" link = "/budget" image = {require("../images/sidebar/dashboard.png")} />
             </div>
 
             <div className = "bottom-options">
-                <div className="option">
-                    <div className="sidebar-icons">
-                        <img src="https://i.ibb.co/1mywcVC/image-removebg-preview-3.png" alt="settings" />
-                    </div>
-                    <Link to ="/settings">Settings</Link>
-                </div>
-                <div className="option">
-                    <Link to ="/">Log Out</Link>
-                </div>
+                <Option title = "Settings" link = "/settings" image = {require("../images/sidebar/settings.png")} />
+                <Option title = "Sign out" link = "/" image = {require("../images/sidebar/dashboard.png")} />
             </div>
         </nav>
     );
