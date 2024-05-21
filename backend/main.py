4fe569362dd131f0ea8b02b01603d70dc872b271
Kelly Do <kellydo8467@gmail.com>
2024-05-21 09:47:17 -0400

    moved code into backend folder

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,31 @@
-from flask import Flask
+# from flask import Flask
+# from flask_cors import CORS
+# from flask import render_template, jsonify
+
+# app = Flask(__name__)
+# cors = CORS(app, origins = "*")
+
+# @app.route("/homess", methods = ["GET"])
+# def home():
+#     return jsonify({"message": "greeings!"}), 200
+
+# if __name__ == "__main__":
+#     app.run(debug = True)
+
+from flask import Flask, jsonify
 from flask_cors import CORS
-from flask import render_template, jsonify
-
+ 
 app = Flask(__name__)
-cors = CORS(app, origins = "*")
-
-@app.route("/homess", methods = ["GET"])
-def home():
-    return jsonify({"message": "greeings!"}), 200
-
-if __name__ == "__main__":
-    app.run(debug = True)
+CORS(app)
+ 
+@app.route('/')
+def index():
+    mydict = {
+        "hello": "hi",
+        "was": "up"
+    }
+    return jsonify(mydict)
+ 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+ 
