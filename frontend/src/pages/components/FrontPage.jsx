8acf4da314a39d114c9802699aa82d98552fe38c
steviels <118238179+steviels@users.noreply.github.com>
2024-05-23 11:10:22 -0400

    navbar

--- frontend/src/pages/components/FrontPage.jsx
+++ frontend/src/pages/components/FrontPage.jsx
@@ -1,18 +1,40 @@
-import Navbar from "@components/Navbar";
-import Footer from "@components/Footer";
+//import Navbar from "@components/Navbar";
+import { Link } from "react-router-dom";
 
 import "@styles/components/FrontPage.scss";
+
+const Navbar2 = () => {
+    return (
+        <nav className = "navbar">
+            <Link className = "logo" to = "/">Expenses</Link>
+
+            <ul>
+                <li><Link to = "about">About</Link></li>
+                <li><Link to = "login">Login</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>footer here!</h1>
+        </footer>
+    )
+}
 
 const Layout = ({ className, children, excludeNav, excludeFt }) => {
     console.log(typeof excludeFt);
     excludeNav = (!excludeNav || excludeNav.toLowerCase() == "false") ? false : true;
     excludeFt = (!excludeFt || excludeFt.toLowerCase() == "false") ? false : true;
+    
     const layoutClassName = "front-page-layout" + (excludeNav ? " no-nav" : "") + (excludeFt ? " no-footer" : "");
     const bodyClassName = "front-page-body" + (className ? " " + className : "");
 
     return (
         <div className = {layoutClassName}>
-            {excludeNav ? null : <Navbar />}
+            {excludeNav ? null : <Navbar2 />}
             <div className = {bodyClassName}>
                 { children } 
             </div>
@@ -22,45 +44,3 @@ const Layout = ({ className, children, excludeNav, excludeFt }) => {
 }
 
 export default Layout;
-
-/*
-const LayoutBodyContainer = ({ className, children }) => {
-    if (className)
-        className = " " + className;
-
-    return (
-        <div className = {"front-page-body" + className}>
-            { children }
-        </div>
-    )
-}
-
-const LayoutWithFooter = ({ className, children }) => {
-    return (
-        <>
-            <Navbar />
-            <LayoutBodyContainer className = {className}>{ children }</LayoutBodyContainer>
-            <Footer />
-        </>
-    )
-}
-
-const LayoutWithoutFooter = ({ className, children }) => {
-    return (
-        <>
-            <Navbar />
-            <LayoutBodyContainer className = {className}>{ children }</LayoutBodyContainer>
-        </>
-    )
-}
-
-const LayoutWithoutNavbar = ({ className, children }) => {
-    return (
-        <LayoutBodyContainer className = {className}>{ children }</LayoutBodyContainer>
-    );
-}
-
-const Layout = LayoutWithFooter;
-export default Layout;
-export { LayoutWithFooter, LayoutWithoutFooter, LayoutWithoutNavbar };
-*/
