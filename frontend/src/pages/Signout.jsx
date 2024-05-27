f8dcf3ba65758df421bf627371fbb8dc7dbfb52c
steviels <118238179+steviels@users.noreply.github.com>
2024-05-27 03:31:40 -0400

    signout

--- frontend/src/pages/Signout.jsx
+++ frontend/src/pages/Signout.jsx
@@ -1,11 +1,37 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { useEffect } from "react";
+
+import { PATHS } from "@/App";
 
 const Signout = () => {
-    return (
-        <>
-            <Navigate to = "/" />
-        </>
-    )
+    const outletContext = useOutletContext();
+    const setLoggedIn = outletContext["setLoggedIn"];
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        fetch("/logout", {
+            method: "POST",
+            credentials: "include",
+        }).then(response => {
+            if (response.ok) {
+                setLoggedIn(false);
+                navigate(PATHS.LoginPath, {"replace": true});
+                return null;
+            } else {
+                return response.json();
+            }
+        }).then(error => {
+            if (error) {
+                alert(error.message)
+            }
+        }).catch(error => {
+            if (error) {
+                alert(error);
+            }
+        });
+    });
+
+    return (<></>)
 }
 
 export default Signout;
