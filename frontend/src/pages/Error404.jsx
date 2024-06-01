9f31d2a5878a44773ca2fa2158896da806d7c313
steviels <118238179+steviels@users.noreply.github.com>
2024-06-01 03:16:27 -0400

    error404 revamp

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -6,11 +6,17 @@ const Error404 = () => {
     const navigate = useNavigate();
 
     return (
-        <>
-            <h1>error404 page</h1>
-            <h1>here's a btn to go back</h1>
-            <button onClick = {() => {navigate(-1)}}>go back!</button>
-        </>
+        <div className = "error404-container">
+            <h1>Oops!</h1>
+
+            <h2>Looks like you're lost</h2>
+            <h3>The page you were looking for is not available.</h3>
+
+            <button className = "primary-btn" onClick = {() => {navigate(-1)}}>Return to Previous Page</button>
+        </div>
+            // <h1>error404 page</h1>
+            // <h1>here's a btn to go back</h1>
+            // <button onClick = {() => {navigate(-1)}}>go back!</button>
     )
 }
 
