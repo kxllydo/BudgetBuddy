739230fa305d517d61761e08e81bc3ef2d8f3dd8
steviels <118238179+steviels@users.noreply.github.com>
2024-06-03 10:34:12 -0400

    uninstall flask mail

--- backend/config.py
+++ backend/config.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_mail import Mail
+from dotenv import load_dotenv
+import os 
 
 from datetime import timedelta
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "dualmonitor"
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
-CORS(app, supports_credentials=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, supports_credentials=True)
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.getenv("SMTP_USER")
+app.config["MAIL_PASSWORD"] = os.getenv("SMTP_PASS")
+mail = Mail(app)
 
 @app.after_request
 def after_request(response):
